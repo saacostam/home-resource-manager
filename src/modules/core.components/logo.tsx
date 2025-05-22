22c2d3d9bcbb6136454b490cf3295d7db5f62684
saacostam <73088662+saacostam@users.noreply.github.com>
2025-05-22 02:10:52 -0500

    feat: add tooltip to logo

--- src/modules/core.components/logo.tsx
+++ src/modules/core.components/logo.tsx
@@ -1,15 +1,17 @@
-import { Flex, ThemeIcon, Title } from "@mantine/core";
+import { Flex, ThemeIcon, Title, Tooltip } from "@mantine/core";
 import { HomeModernIcon } from "../icons";
 
 export function Logo() {
   return (
-    <Flex direction="row" align="center" gap="xs">
-      <ThemeIcon size="sm" variant="white" radius="sm" color="violet">
-        <HomeModernIcon />
-      </ThemeIcon>
-      <Title order={2} size="h5" c="violet">
-        Home Resource Manager
-      </Title>
-    </Flex>
+    <Tooltip label="Mock App for Practicing Mantine Library Composition">
+      <Flex direction="row" align="center" gap="xs">
+        <ThemeIcon size="sm" variant="white" radius="sm" color="violet">
+          <HomeModernIcon />
+        </ThemeIcon>
+        <Title order={2} size="h5" c="violet">
+          Home Resource Manager
+        </Title>
+      </Flex>
+    </Tooltip>
   );
 }
--- src/modules/icons/components/index.ts
+++ src/modules/icons/components/index.ts
@@ -2,5 +2,6 @@ export * from "./check-circle.icon";
 export * from "./chevron-right.icon";
 export * from "./exclamation-circle.icon";
 export * from "./home-modern.icon";
+export * from "./information-circle.icon";
 export * from "./no-symbol.icon";
 export * from "./rocket-launch.icon";
