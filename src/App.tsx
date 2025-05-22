f3b570994fdf54f31a0d9f23b90e2ce836ed849c
saacostam <73088662+saacostam@users.noreply.github.com>
2025-05-22 00:14:13 -0500

    feat: add app-shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
+import { AppShell } from "./modules/layout";
 
 function App() {
   return (
     <MantineProvider>
-      <p>Hello World</p>
+      <AppShell>
+        <p>Hello World</p>
+      </AppShell>
     </MantineProvider>
   );
 }
---
+++ src/modules/layout/components/app-shell.tsx
@@ -0,0 +1,52 @@
+import { useDisclosure } from "@mantine/hooks";
+import {
+  AppShell as MantineAppShell,
+  Burger,
+  Group,
+  Skeleton,
+} from "@mantine/core";
+import type { PropsWithChildren } from "react";
+
+export function AppShell({}: PropsWithChildren) {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
+  return (
+    <MantineAppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <MantineAppShell.Header>
+        <Group h="100%" px="md">
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
+          Header
+        </Group>
+      </MantineAppShell.Header>
+      <MantineAppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </MantineAppShell.Navbar>
+      <MantineAppShell.Main>Main</MantineAppShell.Main>
+    </MantineAppShell>
+  );
+}
