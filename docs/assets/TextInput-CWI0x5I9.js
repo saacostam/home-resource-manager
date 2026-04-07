29c401d0b581224fc98983abdc324b1652a9492e
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-06 22:06:42 -0500

    chore(build): release demo video in landing page

---
+++ docs/assets/TextInput-CWI0x5I9.js
@@ -0,0 +1,1 @@
+import{f as p,b as a,j as r}from"./index-gcB9JQ8v.js";import{a as t}from"./Alert-DXBHDgmM.js";const s=p((e,n)=>{const o=a("TextInput",null,e);return r.jsx(t,{component:"input",ref:n,...o,__staticSelector:"TextInput"})});s.classes=t.classes;s.displayName="@mantine/core/TextInput";export{s as T};
